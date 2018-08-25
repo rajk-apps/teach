16b3ec214a9b31965d686558a3d480609021e85d
Endre Mark Borza <endremborza@gmail.com>
2018-08-25 20:46:51 +0200

    revealjs from additional content

--- teach/static/teach/js/reveal-init.js
+++ teach/static/teach/js/reveal-init.js
@@ -1,5 +1,5 @@
 //Reveal.initialize();
-Reveal.initialize({
+Reveal.configure({
 
     // Display presentation control arrows
     controls: true,
@@ -122,26 +122,4 @@ Reveal.initialize({
 
     // The display mode that will be used to show slides
     display: 'block',
-    
-    dependencies: [
-        // Cross-browser shim that fully implements classList - https://github.com/eligrey/classList.js/
-        //{ src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
-
-        // Interpret Markdown in <section> elements
-        { src: '../../static/teach/plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-        { src: '../../static/teach/plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-
-        // Syntax highlight for <code> elements
-        //{ src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
-
-        // Zoom in and out with Alt+click
-        //{ src: 'plugin/zoom-js/zoom.js', async: true },
-
-        // Speaker notes
-        { src: '../../static/teach/plugin/notes/notes.js', async: true },
-
-        // MathJax
-        { src: '../../static/teach/plugin/math/math.js', async: true }
-    ]
-
 });
--- teach/views.py
+++ teach/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 #from django.contrib.auth.decorators import login_required
 
-from .models import Course,Lecture
+from .models import Course,Lecture,Topic
 
 #@login_required
 def home(request):
@@ -12,6 +12,8 @@ def home(request):
 def course(request,course_id):
     course = get_object_or_404(Course, id=course_id)
     course_structure = course.coursestructure_set.order_by('ordernum')
+    to_include = ['Definition','Task']
+    '''
     #a little crazy list comprehension
     #collecting list of content for each lecture in
     #{'o':ordernum,'c':content} format
@@ -27,8 +29,6 @@ def course(request,course_id):
     #additional content in list of dicts
     acl = [{'o':ac.afterlecture,'c':ac.content} for ac in 
           course.additionalcontent_set.order_by('afterlecture')]
-    
-    to_include = ['Definition','Task']
     
     #type/topic/content
     all_content = [{'name':ctype,'content':[]} for ctype in to_include]
@@ -64,12 +64,72 @@ def course(request,course_id):
         else:
             add_to_all(scl[sc_i]['c'],all_content)
             sc_i += 1
-    
+    '''
     
     return render(request, 'teach/course.html',
                   {'course_name': course.name,
-                   'course_structure':course_structure,
-                   'content':all_content})
+                   'course_id': course_id,
+                   'course_structure': course_structure,
+                   'to_include': to_include})
+
+#@login_required
+def contentshow(request,course_id,type_id):
+    course = get_object_or_404(Course, id=course_id)
+    course_structure = course.coursestructure_set.order_by('ordernum')
+    #a little crazy list comprehension
+    #collecting list of content for each lecture in
+    #{'o':ordernum,'c':content} format
+    scl = [ {'o':d['o'],'c':c} for d in
+           [{'o':cs.ordernum,'cl':[ss.content for ls in
+                cs.lecture.lecturestructure_set.order_by('ordernum',
+                                                         'subordernum')
+                for ss in
+                ls.slide.slidestructure_set.order_by('ordernum')]} for cs in 
+                course_structure] for c in d['cl']]
+    
+    
+    #additional content in list of dicts
+    acl = [{'o':ac.afterlecture,'c':ac.content} for ac in 
+          course.additionalcontent_set.order_by('afterlecture')]
+    
+    
+    level1 = {'name':type_id,'content':[]}
+    
+    def add_to_level(elem,l1):
+        if l1['name'] == elem.get_type_display():
+            added = False
+            for l2 in l1['content']:
+                if l2['name'] == elem.topic.name:
+                    l2['content'].append(elem)
+                    added = True
+            if not added:
+                l1['content'].append({'name':elem.topic.name,
+                                      'description':elem.topic.description,
+                                      'content':[elem]})
+        
+    ac_i = 0
+    
+    sc_i = 0
+    
+    while ac_i < len(acl) and sc_i < len(scl):
+        if acl[ac_i]['o'] < scl[sc_i]['o']:
+            add_to_level(acl[ac_i]['c'],level1)
+            ac_i += 1
+        else:
+            add_to_level(scl[sc_i]['c'],level1)
+            sc_i += 1
+    
+    while ac_i < len(acl) or sc_i < len(scl):
+        if ac_i < len(acl):
+            add_to_level(acl[ac_i]['c'],level1)
+            ac_i += 1
+        else:
+            add_to_level(scl[sc_i]['c'],level1)
+            sc_i += 1
+    
+    
+    return render(request, 'teach/contentshow.html',
+                  {'level1':level1})
 
 #@login_required
 def slideshow(request,lecture_id):
