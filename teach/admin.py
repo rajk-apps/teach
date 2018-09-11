b7cbb262ada4499999821611b591337f0c3e7a68
Endre Mark Borza <endremborza@gmail.com>
2018-09-11 16:55:23 +0200

    tweaking course creation through admin site

--- teach/admin.py
+++ teach/admin.py
@@ -1,7 +1,55 @@
 from django.contrib import admin
 from . import models
+from django.forms import TextInput, Textarea
+from django.db import models as dbmodels
+
 
 # Register your models here.
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Content)
+class ContentAdmin(admin.ModelAdmin):
+    #formfield_overrides = {
+        #models.CharField: {'widget': TextInput(attrs={'size':'20'})},
+        #dbmodels.TextField: {'widget': Textarea(attrs={'rows':20, 'cols':40})},
+    #}
+    pass
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.Slide)
+class SlideAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+@admin.register(models.CourseStructure)
+class CourseStructureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.AdditionalContent)
+class AddContentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.LectureStructure)
+class LectureStructureAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.SlideStructure)
+class SlideStructureAdmin(admin.ModelAdmin):
+    pass
+
+
 
 
 # Functions for importing courses:
